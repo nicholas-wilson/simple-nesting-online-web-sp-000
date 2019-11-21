0de8eaa97fe983b1d8f371cf2c38fc28514bf736
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-21 02:04:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto] = {}
 	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 	programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 	puts programmer_hash[:yukihiro_matsumoto]
